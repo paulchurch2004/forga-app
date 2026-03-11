bf3ab22540cbe7572e4b3b2d32206e313bc620c1
Macintosh <macintosh@MacBook-Pro-3.local>
2026-03-11 13:06:22 +0100

    Deploy FORGA

---
+++ sw.js
@@ -0,0 +1,9 @@
+// Unregister self - clear old cache
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+  );
+  self.clients.claim();
+  self.registration.unregister();
+});
